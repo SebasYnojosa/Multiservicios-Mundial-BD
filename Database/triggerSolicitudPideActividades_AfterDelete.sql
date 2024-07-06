984865ac43357c401efdafd759fe1c4e6193060a
SebasYnojosa <emilio240104@gmail.com>
2024-07-06 15:47:11 -0400

    fix en borrado

--- Database/triggerSolicitudPideActividades_AfterDelete.sql
+++ Database/triggerSolicitudPideActividades_AfterDelete.sql
@@ -3,10 +3,10 @@ ON SolicitudPideActividades AFTER DELETE
 AS
 BEGIN 
     UPDATE SolicitudServicios 
-    SET Costo = Costo - (SELECT Costo FROM Actividad WHERE CodActividad = inserted.CodActividad)
+    SET Costo = Costo - (SELECT Costo FROM Actividad WHERE CodActividad = deleted.CodActividad)
     WHERE CodFicha IN (SELECT CodFicha FROM inserted)
 
     UPDATE SolicitudServicios
     SET CantActividades = CantActividades - 1
-    WHERE CodFicha IN (SELECT CodFicha FROM inserted)
+    WHERE CodFicha IN (SELECT CodFicha FROM deleted)
 END;
