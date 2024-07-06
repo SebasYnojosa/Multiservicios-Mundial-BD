90a3f07f73418f588a8db8de324d1425162f4518
SebasYnojosa <emilio240104@gmail.com>
2024-07-06 03:30:46 -0400

    Ejemplo de delete y put

--- Server/routes/Multiservicios.routes.js
+++ Server/routes/Multiservicios.routes.js
@@ -39,4 +39,29 @@ router.post('/', (req, res) => {
     })
 })
 
+router.delete('/:RIF', (req, res) => {
+    const { RIF } = req.params;
+    new sql.Request().query(`DELETE FROM Multiservicios WHERE RIF = ${RIF}`, (err, data) => {
+        if (err) {
+            console.log('Error executing query: ' + err);
+        }
+        else {
+            res.send('Multiservicio eliminado');
+        }
+    })
+})
+
+router.put('/:RIF', (req, res) => {
+    const { RIF } = req.params;
+    const { Nombre, Ciudad, CIEncargado } = req.body;
+    new sql.Request().query(`UPDATE Multiservicios SET Nombre = '${Nombre}', Ciudad = '${Ciudad}', CIEncargado = ${CIEncargado} WHERE RIF = ${RIF}`, (err, data) => {
+        if (err) {
+            console.log('Error executing query: ' + err);
+        }
+        else {
+            res.send('Multiservicio actualizado');
+        }
+    })
+})
+
 module.exports = router;
