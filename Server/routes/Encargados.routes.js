4f3e9cf6ef28af3c569679e4ef360553639d9444
aepatino21 <aepatino.21@est.ucab.edu.ve>
2024-07-06 22:28:26 -0700

    Fix encargados route

--- Server/routes/Encargados.routes.js
+++ Server/routes/Encargados.routes.js
@@ -16,53 +16,66 @@ router.get('/', (req, res) => {
 
 router.get('/:CIEncargado', (req, res) => {
     const { CIEncargado } = req.params;
-    new sql.Request().query(`SELECT * FROM Encargados WHERE CIEncargado = ${CIEncargado}`, (err, data) => {
-        if (err) {
-            console.log('Error executing query: ' + err);
-        }
-        else {
-            res.send(data.recordset);
-            console.dir(data.recordset);
-        }
-    })
+    let query = `SELECT * FROM Encargados WHERE CIEncargado = @CIEncargado`;
+    new sql.Request()
+        .input('CIEncargado', sql.Int, CIEncargado)
+        .query(query, (err, data) => {
+            if (err) {
+                console.log('Error executing query: ' + err);
+            }
+            else {
+                res.send(data.recordset);
+                console.dir(data.recordset);
+            }
+        })
 })
 
 router.post('/', (req, res) => {
     const { CIEncargado, FInicioEncargado } = req.body;
-    new sql.Request().query(`INSERT INTO Encargados VALUES (${CIEncargado}, '${FInicioEncargado}')`, (err, data) => {
-        if (err) {
-            console.log('Error executing query: ' + err);
-        }
-        else {
-            res.send('Encargado agregado');
-        }
-    })
+    let query = `INSERT INTO Encargados VALUES (@CIEncargado, @FInicioEncargado)`;
+    new sql.Request()
+        .input('CIEncargado', sql.Int, CIEncargado)
+        .input('FInicioEncargado', sql.Date, FInicioEncargado)
+        .query(query, (err, data) => {
+            if (err) {
+                console.log('Error executing query: ' + err);
+            }
+            else {
+                res.send('Encargado agregado');
+            }
+        })
 })
 
 router.delete('/:CIEncargado', (req, res) => {
     const { CIEncargado } = req.params;
-    new sql.Request().query(`DELETE FROM Encargados WHERE CIEncargado = ${CIEncargado}`, (err, data) => {
-        if (err) {
-            console.log('Error executing query: ' + err);
-        }
-        else {
-            res.send('Encargado eliminado');
-        }
-    })
+    let query = `DELETE FROM Encargados WHERE CIEncargado = @CIEncargado`;
+    new sql.Request()
+        .input('CIEncargado', sql.Int, CIEncargado)
+        .query(query, (err, data) => {
+            if (err) {
+                console.log('Error executing query: ' + err);
+            }
+            else {
+                res.send('Encargado eliminado');
+            }
+        })
 })
 
 router.put('/:CIEncargado', (req, res) => {
     const { CIEncargado } = req.params;
     const { FInicioEncargado } = req.body;
-    new sql.Request().query(`UPDATE Encargados SET FInicioEncargado = '${FInicioEncargado} WHERE CIEncargado = ${CIEncargado}`, (err, data) => {
-        if (err) {
-            console.log('Error executing query: ' + err);
-        }
-        else {
-            res.send('Encargado actualizado');
-        }
-    })
+    let query = `UPDATE Encargados SET FInicioEncargado = @FInicioEncargado WHERE CIEncargado = @CIEncargado`;
+    new sql.Request()
+        .input('CIEncargado', sql.Int, CIEncargado)
+        .input('FInicioEncargado', sql.Date, FInicioEncargado)
+        .query(query, (err, data) => {
+            if (err) {
+                console.log('Error executing query: ' + err);
+            }
+            else {
+                res.send('Encargado actualizado');
+            }
+        })
 })
 
 module.exports = router;
-
